9a14ba3b21b4064ac32b4ad59b9eac22562f1ba9
Drew Volz <volz.drew@gmail.com>
2019-09-07 18:51:02 -0700

    Cleanup semicolon in ios module

--- module.ios.js
+++ module.ios.js
@@ -4,7 +4,8 @@ import { NativeModules } from 'react-native'
 const RNAudioStream = NativeModules.RNAudioStream
 
 export function play(url: string): Promise<boolean> {
-    return RNAudioStream.play(url);
+    return RNAudioStream.play(url)
+}
 
 export function pause(): Promise<boolean> {
     return RNAudioStream.pause()
